80ebd2976406f2d3eafa52a37d02e69c7b2e2914
youxiang <673143107@qq.com>
2023-11-06 11:09:37 +0800

    finsh soil

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ const textStyle18White = TextStyle(
 
 const textStyle16grey = TextStyle(
   fontSize: 16,
-  color: Color.fromRGBO(255, 255, 255, 0.5),
+  color: Color.fromRGBO(255, 255, 255, 0.44),
   fontWeight: FontWeight.w500,
   height: 19 / 16
 );
@@ -116,13 +116,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  Widget Circle(){
+  Widget Circle(String color){
     return Container(
       width: 7,
       height: 7,
       decoration: BoxDecoration(
           shape: BoxShape.circle,
-          color: Color.fromRGBO(118, 212, 129, 1)),
+          color: color == 'green'?Color.fromRGBO(118, 212, 129, 1):Colors.white),
     );
   }
 
@@ -148,10 +148,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      SizedBox(height: 9,),
-                      Image.asset('images/icon-environment.png',width: 152,height: 105,),
-                      Text('Environment',style: textStyle22White,),
-                      SizedBox(height: 3,),
+                      Image.asset('images/icon-soil.png',width: 152,height: 118,),
+                      Text('Soil',style: textStyle22White,),
+                      SizedBox(height: 7,),
                       Text('Brand Name',style: textStyle13grey,),
                       Container(
                         width: 80,
@@ -161,15 +160,15 @@ class _MyHomePageState extends State<MyHomePage> {
                           children: [
                             Row(
                               children: [
-                                Circle(),
+                                Circle('white'),
                                 SizedBox(width: 5,),
-                                Text('ONline',style: textStyle16White,),
+                                Text('Disuse',style: textStyle16White,),
                               ],
                             ),
-                            SizedBox(height: 1,),
+                            SizedBox(height: 5,),
                             Row(
                               children: [
-                                Circle(),
+                                Circle('green'),
                                 SizedBox(width: 5,),
                                 Text('1148kwh',style: textStyle16green,),
                               ],
@@ -177,16 +176,6 @@ class _MyHomePageState extends State<MyHomePage> {
                           ],
                         ),
                       ),
-                      Container(
-                        width: 34,
-                        height: 34,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color.fromRGBO(136, 194, 144, 1),
-                        ),
-                        child: Icon(Icons.power_settings_new,size: 28,),
-                      )
                     ],
                   ),
                   SizedBox(width: 26,),
@@ -196,156 +185,38 @@ class _MyHomePageState extends State<MyHomePage> {
                       SizedBox(height: 39,),
                       Text('( Zone 1 )',style: textStyle18White,),
                       Container(
+                        height: 48,
                         margin: EdgeInsets.only(top: 21),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('EC',style: textStyle16grey,),
+                            Text('--',style: textStyle24White,),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 48,
+                        margin: EdgeInsets.only(top: 26),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('PH',style: textStyle16grey,),
+                            Text('--',style: textStyle24White,),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 48,
+                        margin: EdgeInsets.only(top: 26),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text('Temperature',style: textStyle16grey,),
-                            SizedBox(height: 2,),
-                            Container(
-                              height: 29,
-                              child: Row(
-                                children: [
-                                  Text('24',style: textStyle24White,),
-                                  Container(
-                                    height: 29,
-                                    padding: EdgeInsets.only(top: 2.5,left: 2),
-                                    alignment: Alignment.topLeft,
-                                    child: Text('\u2103',style: textStyle16White,),
-                                  )
-                                ],
-                              ),
-                            ),
+                            Text('--',style: textStyle24White,),
                           ],
                         ),
                       ),
-                      Container(
-                        margin: EdgeInsets.only(top: 24),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Rainfall',style: textStyle16grey,),
-                            SizedBox(height: 2,),
-                            Container(
-                              height: 29,
-                              child: Row(
-                                children: [
-                                  Text('0.5',style: textStyle24White,),
-                                  Container(
-                                    height: 29,
-                                    padding: EdgeInsets.only(left: 2,top: 0.5),
-                                    alignment: Alignment.topLeft,
-                                    child: Text('mm',style: textStyle15White,),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 24),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Brightness',style: textStyle16grey,),
-                            SizedBox(height: 2,),
-                            Container(
-                              height: 29,
-                              child: Row(
-                                children: [
-                                  Text('200',style: textStyle24White,),
-                                  Container(
-                                    height: 29,
-                                    padding: EdgeInsets.only(top: 3.5,left: 2),
-                                    alignment: Alignment.topLeft,
-                                    child: Text('Lx',style: textStyle14White,),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(width: 44,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 57,),
-                      Container(
-                        margin: EdgeInsets.only(top: 21),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Humidity',style: textStyle16grey,),
-                            SizedBox(height: 2,),
-                            Container(
-                              height: 29,
-                              child: Row(
-                                children: [
-                                  Text('80',style: textStyle24White,),
-                                  Container(
-                                    height: 29,
-                                    padding: EdgeInsets.only(top: 4,left: 2),
-                                    alignment: Alignment.topLeft,
-                                    child: Text('%',style: textStyle17White,),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 24),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Airflow',style: textStyle16grey,),
-                            SizedBox(height: 2,),
-                            Container(
-                              height: 29,
-                              child: Row(
-                                children: [
-                                  Text('3-4',style: textStyle24White,),
-                                  Container(
-                                    height: 29,
-                                    padding: EdgeInsets.only(left: 2,top: 3),
-                                    alignment: Alignment.topLeft,
-                                    child: Text('Levle',style: textStyle14White,),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 24),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Pressure',style: textStyle16grey,),
-                            SizedBox(height: 2,),
-                            Container(
-                              height: 29,
-                              child: Row(
-                                children: [
-                                  Text('101',style: textStyle24White,),
-                                  Container(
-                                    height: 29,
-                                    padding: EdgeInsets.only(top: 4,left: 2),
-                                    alignment: Alignment.topLeft,
-                                    child: Text('KPA',style: textStyle14White,),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
                     ],
                   ),
                 ],
